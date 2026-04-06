f842f58c2dd0a2997995c1aad74cbffe03767cdb
Alberto Federico Turelli <alberto.turelli@gmail.com>
2026-04-06 18:09:13 +0200

    BASEDIR updated, scripts updated

--- DeployMaintenanceScripts/update_scripts.sh
+++ DeployMaintenanceScripts/update_scripts.sh
@@ -2,14 +2,14 @@
 #
 # update_scripts.sh - Updates SQL Server scripts
 #
-export SOURCE_DIR=/mnt/c/Work/Scripts.SQL
+export SOURCE_DIR="/mnt/c/Work.Git/Scripts.SQL"
 export DESTINATION_DIR=.
 
-cp -a $SOURCE_DIR/DarlingData/Install-All/DarlingData.sql $DESTINATION_DIR/scripts/
-cp -a $SOURCE_DIR/sp_CheckBackup/sp_CheckBackup.sql $DESTINATION_DIR/scripts_common/
-cp -a $SOURCE_DIR/sp_CheckSecurity/sp_CheckSecurity.sql $DESTINATION_DIR/scripts_common/
-cp -a $SOURCE_DIR/sp_CheckTempdb/sp_CheckTempdb.sql $DESTINATION_DIR/scripts_common/
-cp -a $SOURCE_DIR/sp_whoisactive/sp_WhoIsActive.sql $DESTINATION_DIR/scripts_common/
-#cp -a $SOURCE_DIR/SQL-Server-First-Responder-Kit/sp*.sql $DESTINATION_DIR/scripts/
-cp -a $SOURCE_DIR/SQL-Server-First-Responder-Kit/Install-All-Scripts.sql $DESTINATION_DIR/scripts/
-cp -a $SOURCE_DIR/sql-server-maintenance-solution/MaintenanceSolution.sql $DESTINATION_DIR/scripts_common/
+cp -a "$SOURCE_DIR/DarlingData/Install-All/DarlingData.sql" "$DESTINATION_DIR/scripts/"
+cp -a "$SOURCE_DIR/sp_CheckBackup/sp_CheckBackup.sql" "$DESTINATION_DIR/scripts_common/"
+cp -a "$SOURCE_DIR/sp_CheckSecurity/sp_CheckSecurity.sql" "$DESTINATION_DIR/scripts_common/"
+cp -a "$SOURCE_DIR/sp_CheckTempdb/sp_CheckTempdb.sql" "$DESTINATION_DIR/scripts_common/"
+cp -a "$SOURCE_DIR/sp_whoisactive/sp_WhoIsActive.sql" "$DESTINATION_DIR/scripts_common/"
+#cp -a "$SOURCE_DIR/SQL-Server-First-Responder-Kit/sp*.sql" "$DESTINATION_DIR/scripts/"
+cp -a "$SOURCE_DIR/SQL-Server-First-Responder-Kit/Install-All-Scripts.sql" "$DESTINATION_DIR/scripts/"
+cp -a "$SOURCE_DIR/sql-server-maintenance-solution/MaintenanceSolution.sql" "$DESTINATION_DIR/scripts_common/"
